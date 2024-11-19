62905085136d2b186566e3ebebc45e857893ccce
Elmura <erbakytelmura@gmail.com>
2024-11-19 22:06:59 +0500

    tapsrma

--- skrip.js
+++ skrip.js
@@ -1,10 +1,13 @@
-function sozder(word1, word2) {
-  if (word1.toLowerCase() === word2.toLowerCase()) {
-      return "Сөздер бірдей";
-  } else {
-      return "Сөздер әртүрлі";
-  }
-}
-console.log(sozder("Javascript", "javascript"));
-console.log(sozder("Hello", "world"));
+def get_language():
+    print("Сіз қандай тілде сөйлейсіз? (Қазақша, Орысша, Ағылшынша)")
+    language = input().lower()
 
+    if language == "қазақша":
+        greet_in_kazakh()
+    elif language == "орысша":
+        greet_in_russian()
+elif language == "ағылшынша":
+        greet_in_english()
+    else:
+        print("Қате енгізу! Өтінеміз, дұрыс тіл таңдаңыз: қазақша, орысша немесе ағылшынша.")
+        get_language()
